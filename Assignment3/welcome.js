7fd6c2dc555c5d66f01c622568b802d04eed2ce1
saksham0204 <67228762+saksham0204@users.noreply.github.com>
2021-02-16 13:28:32 +0530

    welcome js

---
+++ Assignment3/welcome.js
@@ -0,0 +1,9 @@
+while(!firstName){
+    var firstName = prompt("Enter your first name"); 
+}
+
+while(!lastName){
+    var lastName = prompt("Enter your last name");
+}
+
+console.log("Welcome " + firstName + " " + lastName + " to the World of Web");
